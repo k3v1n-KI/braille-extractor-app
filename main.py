6a606307c2cde8ae46b8263f1b3764cd657a868a
Kevin Igweh <kevinigwehik20@gmail.com>
2024-11-08 13:31:43 -0500

    Created separater page for bruna's speech to text

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, flash
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from pydub import AudioSegment
 from braille_to_text import braille_to_text
 from text_to_speech import text_to_speech
 
 app = Flask(__name__)
 app.secret_key = "Something Ominous"
+
+AudioSegment.ffmpeg = "C:/ffmpeg/bin/ffmpeg.exe"  
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -27,6 +31,31 @@ def home():
         flash("Problem processing image file", "error")
     return render_template("index.html")
 
+@app.route("/speech_to_braille", methods=["GET", "POST"])
+def speech_to_braille():
+    if request.method == "POST":
+        audio_file = request.files['audioFile']
+        file_name = audio_file.filename
+        webm_file_path = f"static/audio/{file_name}"
+        audio_file.save(webm_file_path)
+        
+        # Convert .webm to .wav using pydub
+        wav_file_path = os.path.splitext(webm_file_path)[0] + ".wav"
+        try:
+            # Load the .webm file
+            audio = AudioSegment.from_file(webm_file_path, format="webm")
+            # Export the audio as .wav
+            audio.export(wav_file_path, format="wav")
+            flash("File uploaded successfully", "success")
+            # Delete the .webm file after conversion
+            os.remove(webm_file_path)
+            
+        except Exception as e:
+            flash(f"Error converting to wav file: {e}", "error")
+        return render_template("speech_to_braille.html", wav_file_path=wav_file_path)
+        
+    return render_template("speech_to_braille.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
+  
